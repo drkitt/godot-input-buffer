75e1e9718973eb19511a2bdf658837ba29c20b13
Alex Kitt <alex.kitt@outlook.com>
2022-07-27 21:19:40 -0400

    Rudimentary movement

---
+++ Input buffer/Treadmill.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Treadmill : Node2D
+{
+    /// <summary> Pixels per second the ground moves at. </summary>
+    [Export] private float _speed = 10f;
+    /// <summary> Pixels per second per second the ground accelerates at. </summary>
+    [Export] private float _acceleration = 0f;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        Position += Vector2.Left * _speed * delta;
+        _speed += _acceleration * delta;
+    }
+}
