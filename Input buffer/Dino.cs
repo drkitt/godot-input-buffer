e137333d924141c34c652bf099ab68e00f5f0d4d
Alex Kitt <alex.kitt@outlook.com>
2022-07-27 22:11:14 -0400

    Successfully make the worst endless runner ever

--- Input buffer/Dino.cs
+++ Input buffer/Dino.cs
@@ -6,9 +6,19 @@ using System;
 /// </summary>
 public class Dino : Sprite
 {
+    /// <summary> Plays animations. </summary>
     private AnimationPlayer _animator;
+    
+    /// <summary> Pixels per second the dino currently moves downward. </summary>
+    [Export] private float _speed = 0;
+    /// <summary> How many pixels per second squared the dino accelerates towards the ground at. </summary>
+    [Export] private float _gravity = 80;
+    /// <summary> Pixels per second downward the dino moves the moment it jumps. </summary>
+    [Export] private float _initial_jump_speed = -80;
 
-    // Called when the node enters the scene tree for the first time.
+    /// <summary> 
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
         _animator = GetNode<AnimationPlayer>("AnimationPlayer");
@@ -16,12 +26,27 @@ public class Dino : Sprite
         _animator.Play("Run");
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    /// <summary>
+    /// Called every frame.
+    /// </summary>
+    /// <param name="delta"> The elapsed time since the previous frame. </param>
     public override void _Process(float delta)
     {
+        // Apply forces. The dino is considered grounded if its transform's y is at least 0.
+        Position += Vector2.Down * _speed;
+        if (Position.y >= 0)
+        {
+            Position = Vector2.Zero;
+            _speed = 0;
+        }
+        else
+        {
+            _speed += _gravity;
+        }
+        
         if (Input.IsActionJustPressed("ui_select"))
         {
-            GD.Print("jump");
+            _speed = _initial_jump_speed;
         }
     }
 }
--- Input buffer/Treadmill.cs
+++ Input buffer/Treadmill.cs
@@ -11,13 +11,18 @@ public class Treadmill : Node2D
     /// <summary> Pixels per second per second the ground accelerates at. </summary>
     [Export] private float _acceleration = 0f;
 
-    // Called when the node enters the scene tree for the first time.
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
         
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    /// <summary>
+    /// Called every frame.
+    /// </summary>
+    /// <param name="delta"> The elapsed time since the previous frame. </param>
     public override void _Process(float delta)
     {
         Position += Vector2.Left * _speed * delta;
