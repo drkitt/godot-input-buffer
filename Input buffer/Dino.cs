9730b833f5608f28ffd233962d02af9daa131a3d
Alex Kitt <alex.kitt@outlook.com>
2022-07-30 13:09:02 -0400

    Implement short hop

--- Input buffer/Dino.cs
+++ Input buffer/Dino.cs
@@ -18,9 +18,15 @@ public class Dino : KinematicBody2D
     private DinoState _state = DinoState.Idle;
     private AnimationPlayer _animator; [Export] private NodePath _animation_player_path;
     private Vector2 _velocity;
+    private float _gravity;
 
-    /// <summary> How many pixels per second squared the dino accelerates towards the ground at. </summary>
-    [Export] private float _gravity = 2400f;
+    /// <summary> How many pixels per second squared the dino accelerates towards the ground at while rising. </summary>
+    [Export] private float _regular_gravity = 2400f;
+    /// <summary>
+    /// How many pixels per second squared the dino accelerates towards the ground at if the player releases the jump 
+    /// button while rising.
+    /// </summary>
+    [Export] private float _short_hop_gravity = 4800f;
     /// <summary>
     /// Pixels per second downward the dino moves the moment it jumps.
     /// Recall that the coordinate system has the positive y axis point down, so this should be negative.
@@ -51,13 +57,27 @@ public class Dino : KinematicBody2D
                 {
                     _state = DinoState.Jumping;
                     _velocity = _initial_jump_speed * Vector2.Up;
+                    _gravity = _regular_gravity;
                     _animator.Play("Run");
                 }
                 break;
             }
             case DinoState.Jumping:
             {
+                // Short-hop if the player releases the jump button while rising
+                if (Input.IsActionJustReleased(JUMP_ACTION) && _velocity.Dot(Vector2.Up) > 0)
+                {
+                    _gravity = _short_hop_gravity;
+                }
+
                 _velocity += _gravity * delta * Vector2.Down;
+
+                // Reset the gravity once the dino begins falling after a short hop. 
+                if (_velocity.Dot(Vector2.Up) < 0)
+                {
+                    _gravity = _regular_gravity;
+                }
+
                 MoveAndSlide(_velocity, Vector2.Up);
                 if (IsOnFloor())
                 {
@@ -70,11 +90,11 @@ public class Dino : KinematicBody2D
                 if (Input.IsActionJustPressed(JUMP_ACTION))
                 {
                     _velocity = _initial_jump_speed * Vector2.Up;
+                    _gravity = _regular_gravity;
                     _state = DinoState.Jumping;
                 }
                 break;
             }
-
             default: throw new InvalidOperationException("Unhandled state: " + _state);
         }
     }
