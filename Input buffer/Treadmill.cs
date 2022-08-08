71a5b6456e3839f24b685934bb18a2d254e1df4b
Alex Kitt <alex.kitt@outlook.com>
2022-08-07 22:10:20 -0400

    Stop the treadmill by default

--- Input buffer/Treadmill.cs
+++ Input buffer/Treadmill.cs
@@ -10,10 +10,9 @@ public class Treadmill : Node2D
     [Export] private float _speed = 10f;
     /// <summary> Pixels per second per second the ground accelerates at. </summary>
     [Export] private float _acceleration = 0f;
-    /// <summary>
-    /// The sprites used for the ground.
-    /// </summary>
+    /// <summary> The sprites used for the ground. </summary>
     private Node2D _ground1, _ground2; [Export] private NodePath _groundPath1, _groundPath2;
+    private bool _moving = false;
 
     /// <summary>
     /// Called when the node enters the scene tree for the first time.
@@ -30,9 +29,12 @@ public class Treadmill : Node2D
     /// <param name="delta"> The elapsed time since the previous frame. </param>
     public override void _Process(float delta)
     {
-        _ground1.Position += Vector2.Left * _speed * delta;
-        _ground2.Position += Vector2.Left * _speed * delta;
+        if (_moving)
+        {
+            _ground1.Position += Vector2.Left * _speed * delta;
+            _ground2.Position += Vector2.Left * _speed * delta;
 
-        _speed += _acceleration * delta;
+            _speed += _acceleration * delta;
+        }
     }
 }
