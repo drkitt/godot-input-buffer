70588aa4de072db662d3fd635f603f746931413d
Alex Kitt <alex.kitt@outlook.com>
2022-08-15 21:01:14 -0400

    Fix comment formatting

--- Input buffer/NodeExtensions.cs
+++ Input buffer/NodeExtensions.cs
@@ -11,7 +11,7 @@ namespace ExtensionMethods
         /// <summary>
         /// Gets all of the node's children that are of the specified type
         /// </summary>
-        /// <param name="node">  The object to call this method for. </param>
+        /// <param name="node"> The object to call this method for. </param>
         /// <typeparam name="T"> The type to look for. </typeparam>
         /// <returns> Returns an array of references to node's children. </returns>
         public static Godot.Collections.Array<T> GetChildrenOfType<T>(this Node node) where T : Node
