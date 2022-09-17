beaa3200541e7e3faddf7fa82de1f762968969af
Alex Kitt <alex.kitt@outlook.com>
2022-09-16 21:35:43 -0500

    Change root node from Node2D to Node

--- Input buffer/DinoGame.cs
+++ Input buffer/DinoGame.cs
@@ -10,22 +10,22 @@ using System;
 /// objects communicate with each other using their signals directly. So, there's gonna be coupling in the editor either
 /// way, and I'd rather put it all in an AnimationPlayer than spread it across the scene. Plus, putting it in an 
 /// animation makes it waaaay easier to manage timing.
-public class DinoGame : Node2D
+public class DinoGame : Node
 {
-    private AnimationPlayer _animator; [Export] private NodePath _animationPlayerPath;
+	private AnimationPlayer _animator; [Export] private NodePath _animationPlayerPath;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        _animator = GetNode<AnimationPlayer>(_animationPlayerPath);
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_animator = GetNode<AnimationPlayer>(_animationPlayerPath);
+	}
 
-    /// <summary>
-    /// Gets the game ready to play. Probably called when receiving a signal from the dino that it's finished its intro 
-    /// animation.
-    /// </summary>
-    private void _on_Dino_IntroJumpFinished()
-    {
-        _animator.Play("Intro animation");
-    }
+	/// <summary>
+	/// Gets the game ready to play. Probably called when receiving a signal from the dino that it's finished its intro 
+	/// animation.
+	/// </summary>
+	private void _on_Dino_IntroJumpFinished()
+	{
+		_animator.Play("Intro animation");
+	}
 }
