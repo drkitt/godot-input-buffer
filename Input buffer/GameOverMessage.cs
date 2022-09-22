cbd25ff840d7ce8e0e6370d2e2bee4d68da92c67
Alex Kitt <alex.kitt@outlook.com>
2022-09-22 17:30:13 -0500

    Add stub retry button

--- Input buffer/GameOverMessage.cs
+++ Input buffer/GameOverMessage.cs
@@ -4,7 +4,7 @@ using System;
 /// <summary>
 /// Pops up when the game is over to let the player know they lost
 /// </summary>
-public class GameOverMessage : TextureRect
+public class GameOverMessage : Control
 {
     private void _on_Dino_GotHit()
     {
