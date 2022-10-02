7503dca3cf24e448d3a148112f64779e51244477
Alex Kitt <alex.kitt@outlook.com>
2022-10-02 15:30:45 -0500

    Fix score blinking endgame behaviour

--- Input buffer/Score.cs
+++ Input buffer/Score.cs
@@ -16,8 +16,26 @@ public class Score : Label
 
     /// <summary> How many points the player gains per second. </summary>
     [Export] private float _speed = 10;
+    /// <summary> Whether changes to the score show up on screen. </summary>
+    [Export] private bool _updating = false;
+    /// <summary> The score display blinks whenever they player scores this many points. </summary>
+    [Export] private float _blinkMilestone = 100;
+    /// <summary> Makes the score blink when the player reaches a milestone. </summary>
+    private AnimationPlayer _animator; [Export] private NodePath _animationPlayerPath;
+    /// <summary> Keeps track of when the score should blink. </summary>
+    private Timer _timer; [Export] private NodePath _timerPath;
+
     private float _score = 0;
     private bool _moving = false;
+
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
+    public override void _Ready()
+    {
+        _animator = GetNode<AnimationPlayer>(_animationPlayerPath);
+        _timer = GetNode<Timer>(_timerPath);
+    }
 
     /// <summary>
     /// Called every frame.
@@ -28,7 +46,10 @@ public class Score : Label
         if (_moving)
         {
             _score += _speed * delta;
+        }
 
+        if (_updating)
+        {
             // Display the score on the label.
             Text = _score.ToString(SCORE_FORMAT);
         }
@@ -38,6 +59,34 @@ public class Score : Label
     public void Start()
     {
         _moving = true;
+        _updating = true;
         _score = 0;
+        _timer.WaitTime = _blinkMilestone / _speed;
+
+        _timer.Start();
+    }
+
+    /// <summary>
+    /// Called when the game ends.
+    /// </summary>
+    private void _on_Dino_GotHit()
+    {
+        // If the score is currently blinking, stop doing that.
+        _timer.Stop();
+        if (_animator.PlaybackActive)
+        {
+            _animator.Stop();
+            Text = _score.ToString(SCORE_FORMAT);
+            Modulate = new Color(0xffffffff);
+        }
+    }
+
+    /// <summary>
+    /// Called when it's time for the score display to blink.
+    /// </summary>
+    private void _on_Timer_timeout()
+    {
+        _animator.Play("Blink");
+        _timer.Start();
     }
 }
