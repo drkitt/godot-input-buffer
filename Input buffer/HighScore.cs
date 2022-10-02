af6b94fea694feb249f6ecd20f7278b0fd5f3598
Alex Kitt <alex.kitt@outlook.com>
2022-10-02 15:29:45 -0500

    Aw man I never added HighScore.cs
    curse you git commit -a

---
+++ Input buffer/HighScore.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Records the best score the player has gotten.
+/// </summary>
+public class HighScore : Label
+{
+    /// <summary> Node keeping track of the score </summary>
+    private Score _scoreNode; [Export] private NodePath _scoreNodePath;
+    private float _highScore = 0;
+
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
+    public override void _Ready()
+    {
+        _scoreNode = GetNode<Score>(_scoreNodePath);
+    }
+
+    /// <summary>
+    /// Called when the game ends.
+    /// </summary>
+    private void _on_Dino_GotHit()
+    {
+        Update(_scoreNode.DisplayedValue);
+    }
+
+    /// <summary>
+    /// Updates the high score.
+    /// </summary>
+    /// <param name="newScore"> 
+    /// The score the player just got. Will be displayed if it's higher than the current high score. 
+    /// </param>
+    private void Update(float newScore)
+    {
+        if (newScore > _highScore)
+        {
+            _highScore = newScore;
+            Text = "HI " + _highScore.ToString("00000");
+        }
+    }
+}
