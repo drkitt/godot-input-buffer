f3af03c6c3134bce4c31c7ae16623bed7063b07b
Alex Kitt <alex.kitt@outlook.com>
2022-10-04 13:16:55 -0500

    Add comment and remove unused methods

--- Input buffer/systems/PauseControl.cs
+++ Input buffer/systems/PauseControl.cs
@@ -2,27 +2,10 @@ using Godot;
 using System;
 
 /// <summary>
-/// 
+/// Pauses the game when the window loses focus.
 /// </summary>
 public class PauseControl : Node
 {
-    /// <summary>
-    /// Called when the node enters the scene tree for the first time.
-    /// </summary>
-    public override void _Ready()
-    {
-
-    }
-
-    /// <summary>
-    /// Called every frame.
-    /// </summary>
-    /// <param name="delta"> The elapsed time since the previous frame. </param>
-    public override void _Process(float delta)
-    {
-
-    }
-
     public override void _Notification(int what)
     {
         base._Notification(what);
