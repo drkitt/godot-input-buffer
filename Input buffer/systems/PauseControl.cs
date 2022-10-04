7860008cc8b4837c616bcd1121bcc8b1c763a3a9
Alex Kitt <alex.kitt@outlook.com>
2022-10-04 13:47:37 -0500

    Add comment to PauseControl._Notification method

--- Input buffer/systems/PauseControl.cs
+++ Input buffer/systems/PauseControl.cs
@@ -6,6 +6,10 @@ using System;
 /// </summary>
 public class PauseControl : Node
 {
+    /// <summary>
+    /// Called by the engine to respond to engine-level callbacks.
+    /// </summary>
+    /// <param name="what"> The notification that the engine has sent this node. </param>
     public override void _Notification(int what)
     {
         base._Notification(what);
